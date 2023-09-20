21d07bb0b3f3efaa052c0d299e04e1f7e168d530
Shaarvari Kiran <shaarvarigowda@gmail.com>
2023-09-20 10:24:21 +0530

    Added tests for my Node.js application

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const app = require('../../app'); // Replace with the actual path to your application's main file.
+
+describe('Your Application', function () {
+  it('should do something', function () {
+    // Your test code here
+    const result = add(2,3);
+    assert.equal(result, 5); // Replace with your actual test logic
+  });
+});
